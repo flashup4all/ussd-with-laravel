bbb8e20d87c19c2328bc54120448e2e4d787aaf4
Asusu03 <flashup4all@gmail.com>
2019-08-22 19:04:33 +0100

    updates

--- app/Http/Controllers/USSD.php
+++ app/Http/Controllers/USSD.php
@@ -14,68 +14,128 @@ class USSD extends Controller
         $serviceCode=$request->input('serviceCode');
         $level = explode("*", $text);
         //if (isset($text)) {
-   
-        if ( $text == "" ) {
-            $response="CON Welcome to the my bank portal.\n";
-            $response .= "1. account balance\n";
-            $response .= "2. Transfer \n";
-            $response .= "3. Fund wallet \n";
-            $response .= "4. Airtime topup \n";
-            $response .= "5. Airtime recharge \n";
-            $response .= "0. Exit";
-        }
-        if(isset($level[0]) && $level[0] == 1  && !isset($level[1]))
+    
+        $user = true;
+        if(!$user)
         {
-            $response="END Account Bal: $500 \n";
-            $response .= "0. back";
+            if ( $text == "" ) {
+                $response="CON please select default language.\n";
+                $response .= "1. English\n";
+                $response .= "2. Hausa \n";
+                $response .= "3. Igbo \n";
+                $response .= "4. Yoruba \n";
+                $response .= "0. Cancel";
+            }
+            if(isset($level[0]) && $level[0] !='')
+            {
+                $response="CON Enter Fullname,age,gender. eg(John Doe, 19, M/F) \n";
+                //$response .= "0. back";
+            }
+            if(isset($level[0]) && $level[0] !='' && isset($level[1]))
+            {
+                $response="CON enter State of residence \n";
+                //$response .= "0. back";
+            }
+            if(isset($level[0]) && $level[0] !='' && isset($level[1]) && $level[1] != '' && isset($level[2]) && $level[2] != '')
+            {
+                $response="CON Enter Enter LGA \n";
+                //$response .= "0. back";
+            }
+            if(isset($level[0]) && $level[0] !=''  && isset($level[1]) && $level[1] != '' && isset($level[2]) && $level[2] != '' && isset($level[3]) && $level[3] != '')
+            {
+                $response="CON Enter address \n";
+                //$response .= "0. back";
+            }
+            if(isset($level[0]) && $level[0] !=''  && isset($level[1]) && $level[1] != '' && isset($level[2]) && $level[2] != '' && isset($level[3]) && $level[3] != '' && isset($level[4]) && $level[4] != '')
+            {
+                $response="END Thank you for Registering! \n";
+                //$response .= "0. back";
+            }
+
         }
-        if(isset($level[0]) && $level[0] == 1  && isset($level[1]) && $level[1] == 0)
+        if($user)
         {
-            $response="CON Welcome to the registration portal.\nPlease enter you full name\n";
-            $response .= "1. account balance\n";
-            $response .= "2. Transfer \n";
-            $response .= "3. Airtime topup \n";
-            $response .= "0. Exit";
-        }
-        if(isset($level[0]) && $level[0] == 2  && !isset($level[1]))
-        {
-            $response="CON Enter Mobile Number \n";
-           
-        }
-        if(isset($level[0]) && $level[0] == 2  && !isset($level[1]))
-        {
-            $response="CON Select bank account \n";
-            $response .= "1. GT Bank \n";
-            $response .= "2. Zenith Bank \n";
-            $response .= "0. back";
-        }
-        
-        if(
-            isset($level[0]) && $level[0] == 2  && isset($level[1]) && strlen((string)$level[1]) > 1 && isset($level[2]) && strlen((string)$level[2]) > 1)
-        {
-           $response="CON enter pin \n";
+            if ( $text == "" ) {
+                $response="CON Welcome to DatPay.\n";
+                $response .= "1. Check Balance\n";
+                $response .= "2. Make Payment \n";
+                $response .= "3. Transfer Funds  \n";
+                $response .= "4. Fund Wallet \n";
+                $response .= "5. Pay Bills \n";
+                $response .= "5. Airtime Recharge \n";
+                $response .= "5. Airtime Topup \n";
+                $response .= "5. Cardless Withdrawal \n";
+                $response .= "0. Exit";
+            }
+
+            switch ($level[0]) {
+                case 1:
+                        $response="END Your account balance is $1000.00 \n";
+                    break;
+                case 2:
+                        $response="CON Enter Account Number \n";
+                        if(isset($level[1]) && $level[1] !='')
+                        {
+                            $response="CON Select Bank \n";
+                            $response .= "1. GT Bank \n";
+                            $response .= "2. Zenith Bank \n";
+                            $response .= "2. Access Bank \n";
+                        }
+                        if(isset($level[1]) && $level[1] !='' && isset($level[2]) && $level[2] !='')
+                        {
+                            $response="CON Enter your 4 digit Pin \n";
+                        }
+                        if(isset($level[1]) && $level[1] !='' && isset($level[2]) && $level[2] !='' && isset($level[3]) && $level[3] !='')
+                        {
+                            $response="END Transfer Successful to John Doe \n";
+                        }
+                    break;
+                default:
+                    # code...
+                    break;
+            }
             
-            // $response .= "0. back";
-        }
-        if(
-            isset($level[0]) && $level[0] == 2  && isset($level[1]) && strlen((string)$level[1]) > 1 && isset($level[2]) && strlen((string)$level[2]) > 1 && isset($level[3]) && strlen((string)$level[3]) > 1)
-        {
-           $response="END Transaction Succesfull \n";
             
-            // $response .= "0. back";
-        }
-        if(isset($level[0]) && $level[0] == 2  && isset($level[1]) && $level[1] == 1 && isset($level[2]))
-        {
-            $response="CON enter account number \n";
+            /*if(isset($level[0]) && $level[0] == 2  && !isset($level[1]))
+            {
+                $response="CON Enter Mobile Number \n";
+               
+            }
+            if(isset($level[0]) && $level[0] == 2  && !isset($level[1]))
+            {
+                $response="CON Select bank account \n";
+                $response .= "1. GT Bank \n";
+                $response .= "2. Zenith Bank \n";
+                $response .= "0. back";
+            }
             
-            $response .= "0. back";
-        }
-        if(isset($level[0]) && $level[0] == 2  && isset($level[1]) && $level[1] == 1 && isset($level[2]) && $level[2] == 0)
-        {
-            $response="CON Select bank account \n";
-            $response .= "1. GT Bank \n";
-            $response .= "2. Zenith Bank \n";
-            $response .= "0. back";
+            if(
+                isset($level[0]) && $level[0] == 2  && isset($level[1]) && strlen((string)$level[1]) > 1 && isset($level[2]) && strlen((string)$level[2]) > 1)
+            {
+               $response="CON enter pin \n";
+                
+                // $response .= "0. back";
+            }
+            if(
+                isset($level[0]) && $level[0] == 2  && isset($level[1]) && strlen((string)$level[1]) > 1 && isset($level[2]) && strlen((string)$level[2]) > 1 && isset($level[3]) && strlen((string)$level[3]) > 1)
+            {
+               $response="END Transaction Succesfull \n";
+                
+                // $response .= "0. back";
+            }
+            if(isset($level[0]) && $level[0] == 2  && isset($level[1]) && $level[1] == 1 && isset($level[2]))
+            {
+                $response="CON enter account number \n";
+                
+                $response .= "0. back";
+            }
+            if(isset($level[0]) && $level[0] == 2  && isset($level[1]) && $level[1] == 1 && isset($level[2]) && $level[2] == 0)
+            {
+                $response="CON Select bank account \n";
+                $response .= "1. GT Bank \n";
+                $response .= "2. Zenith Bank \n";
+                $response .= "0. back";
+            }*/
         }
         
        /* else if(isset($level[2]) && $level[2]!="" && !isset($level[3])){
