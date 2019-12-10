4bb65ae1c4d8cd7db011b199011e915b4ecb326b
Bardeson Lucky <mac@macs-MacBook-Pro.local>
2019-12-10 12:06:53 +0100

    in commerce test

--- app/Http/Controllers/USSD.php
+++ app/Http/Controllers/USSD.php
@@ -42,61 +42,6 @@ class USSD extends Controller
             $response .="Request Confirmed \n";
            
         }
-        /*if(isset($level[0]) && $level[0] == 1  && isset($level[1]) && $level[1] == 0)
-        {
-            $response="CON Welcome to the registration portal.\nPlease enter you full name\n";
-            $response .= "1. account balance\n";
-            $response .= "2. Transfer \n";
-            $response .= "3. Airtime topup \n";
-            $response .= "0. Exit";
-        }
-        if(isset($level[0]) && $level[0] == 2  && !isset($level[1]))
-        {
-            $response="CON Select bank account \n";
-            $response .= "1. GT Bank \n";
-            $response .= "2. Zenith Bank \n";
-            $response .= "0. back";
-        }
-        if(isset($level[0]) && $level[0] == 2  && !isset($level[1]))
-        {
-            $response="CON Select bank account \n";
-            $response .= "1. GT Bank \n";
-            $response .= "2. Zenith Bank \n";
-            $response .= "0. back";
-        }
-        if(isset($level[0]) && $level[0] == 2  && isset($level[1]) && $level[1] == 0)
-        {
-            $response="CON Welcome to the registration portal.\nPlease enter you full name\n";
-            $response .= "1. account balance\n";
-            $response .= "2. Transfer \n";
-            $response .= "3. Airtime topup \n";
-            $response .= "0. Exit";
-        }
-        if(isset($level[0]) && $level[0] == 2  && isset($level[1]) && $level[1] == 1)
-        {
-            $response="CON enter account number \n";
-            
-            $response .= "0. back";
-        }
-        if(isset($level[0]) && $level[0] == 2  && isset($level[1]) && $level[1] == 1 && isset($level[2]) && $level[2] == 0)
-        {
-            $response="CON Select bank account \n";
-            $response .= "1. GT Bank \n";
-            $response .= "2. Zenith Bank \n";
-            $response .= "0. back";
-        }
-        
-        else if(isset($level[2]) && $level[2]!="" && !isset($level[3])){
-            //Save data to database
-            $data=array(
-                'phonenumber'=>$phonenumber,
-                'fullname' =>$level[0],
-                'electoral_ward' => $level[1],
-                'national_id'=>$level[2]
-                );
-            
-            $response="END Thank you ".$level[0]." for registering.\nWe will keep you updated"; 
-        }*/
             header('Content-type: text/plain');
             echo $response;
         //}
