27ea52847f6329ebff558e5fb7f655498b925131
Bardeson Lucky <mac@macs-MacBook-Pro.local>
2019-12-10 11:58:23 +0100

    in commerce test

--- app/Http/Controllers/USSD.php
+++ app/Http/Controllers/USSD.php
@@ -8,26 +8,41 @@ class USSD extends Controller
 {
      public static function ussd(Request $request)
     {
-         $request->all();
-        $text=$request->input('USSD_STRING');
-        $phonenumber=$request->input('MSISDN');
-        $serviceCode=$request->input('serviceCode');
+        //$request->all();
+        $text=$request->input('text');
+        $sessionId = $request->input('sessionId');
+        $phonenumber = $request->input('phoneNumber');
+        $sessionId = $request->input('sessionId');
+        $serviceCode = $request->input('serviceCode');
+        $networkCode = $request->input('networkCode');
         $level = explode("*", $text);
         //if (isset($text)) {
    
         if ( $text == "" ) {
-            $response="CON Welcome to the registration portal.\nPlease enter you full name\n";
-            $response .= "1. account balance\n";
-            $response .= "2. Transfer \n";
-            $response .= "3. Airtime topup \n";
+            $response="CON Welcome Ink\n";
+            $response .= "1. Mobile phones & accessoriese\n";
+            $response .= "2. Heath & Beauty \n";
+            $response .= "3. Home & Kitchen \n";
+            $response .= "4. Fashion \n";
+            $response .= "5. Computer accessories \n";
             $response .= "0. Exit";
         }
-        if(isset($level[0]) && $level[0] == 1  && !isset($level[1]))
+        if(isset($level[0]) /*&& $level[0] == 2*/  && !isset($level[1]))
         {
-            $response="CON Account Bal: $500 \n";
+            $response="CON Health & Beauty \n";
+            $response .="1. Beard Oil \n";
+            $response .="2. Facial Wipe \n";
+            $response .="3. Perfumes \n";
+            $response .="4. Makeup \n";
             $response .= "0. back";
         }
-        if(isset($level[0]) && $level[0] == 1  && isset($level[1]) && $level[1] == 0)
+        if(isset($level[0])/* && $level[0] == 2*/  && isset($level[1]))
+        {
+            $response="END thanks \n";
+            $response .="Request Confirmed \n";
+           
+        }
+        /*if(isset($level[0]) && $level[0] == 1  && isset($level[1]) && $level[1] == 0)
         {
             $response="CON Welcome to the registration portal.\nPlease enter you full name\n";
             $response .= "1. account balance\n";
@@ -81,7 +96,7 @@ class USSD extends Controller
                 );
             
             $response="END Thank you ".$level[0]." for registering.\nWe will keep you updated"; 
-        }
+        }*/
             header('Content-type: text/plain');
             echo $response;
         //}
