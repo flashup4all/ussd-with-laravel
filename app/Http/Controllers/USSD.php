986e9ecfb48793320a320832ce25e8aa63ef1ca8
Bardeson Lucky <flashup4all@gmail.com>
2021-08-07 17:42:49 +0100

    updates

--- app/Http/Controllers/USSD.php
+++ app/Http/Controllers/USSD.php
@@ -15,27 +15,37 @@ class USSD extends Controller
         //if (isset($text)) {
    
         if ( $text == "" ) {
-            $response="CON Welcome John Doe\n";
-            $response .= "1. Account Bal\n";
-            $response .= "2. Transfer \n";
-            $response .= "3. Airtime Recharge \n";
+            $response="CON MOOV\n";
+            $response .= "1. IZY Heures Plus\n";
+            $response .= "2. Moov Folie \n";
+            $response .= "3. Internet Mobile \n";
+            $response .= "4. IZY SMS \n";
+            $response .= "5. International \n";
+            $response .= "6. Roaming \n";
+            $response .= "7. IZY Flex \n";
+            $response .= "8. Offres Perso \n";
             $response .= "0. Exit";
         }
-        if(isset($level[0]) && $level[0] == 1 && !isset($level[1]))
+        if(isset($level[0]) && $level[0] == 3 && !isset($level[1]))
         {
-            $response="END Your account Bal: \n";
-            $response .=" #50,000.00 \n";
+            $response="END INTERNET MOBILE \n";
+            $response .= "1. Acheter un forfait \n";
+            $response .= "2. Consultation volume \n";
+            $response .= "3. Plus d'informations \n";
+            $response .= "4. Acheter pour un tiers \n";
+            $response .= "5. Facebook Flex \n";
+            $response .= "0. RETOUR \n";
         }
-        if(isset($level[0]) && $level[0] == 2 && !isset($level[1]))
-        {
-            $response="CON Select Bank \n";
-            $response .="1. GTB\n";
-            $response .="2. First Bank \n";
-            $response .="3. Access Bank \n";
-            $response .="4. FCMB \n";
-            $response .= "0. back";
-        }
-        if(isset($level[0]) && $level[0] == 2  && isset($level[1]) && !isset($level[2]))
+        // if(isset($level[0]) && $level[0] == 2 && !isset($level[1]))
+        // {
+        //     $response="CON Select Bank \n";
+        //     $response .="1. GTB\n";
+        //     $response .="2. First Bank \n";
+        //     $response .="3. Access Bank \n";
+        //     $response .="4. FCMB \n";
+        //     $response .= "0. back";
+        // }
+        if(isset($level[0]) && $level[0] == 3 && isset($level[1]) && $level[1] == 1 && isset($level[1]) && !isset($level[3]))
         {
             $response="CON enter Acct. No.\n";
            
